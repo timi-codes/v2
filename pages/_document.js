cff7879e2a325bb25537fca040e13a241be35541
Timi Tejumola <tejumoladavid@gmail.com>
2020-05-03 06:30:47 +0100

    implemented basic structure

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+    }
+    render() {
+        return (
+        <Html lang="en">
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;800&display=swap" rel="stylesheet"/>
+                <script dangerouslySetInnerHTML={{ __html: 'try{Typekit.load({ async: false });}catch(e){}' }} />
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+        );
+    }
+}
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,41 @@
 import Head from 'next/head'
+import Avatar from '../components/Avatar';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Timi Tejumola</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        <h1 className="hello">Hello! <span className="emoji wave-hand"></span></h1>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I'm <span>Timi Tejumola</span>, a design-minded software engineer focused on building beautiful interfaces & experiences across mobile & web platforms.🚀
         </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="section-two">
+          <div/>
+          <div className="links">
+            <a
+              href="https://docs.google.com/document/d/1B3m6d2JoUdv5Zb-TwwVKVTotKpSLIXKIAWtu9iyO7lA/edit"
+              className="resume-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >RESUME</a>
+            <ul>
+              <li><a href="https://www.github.com/timi-codes"><img src="/images/github.svg"/></a></li>
+              <li><a href="https://www.linkedin.com/timitejumola"><img src="/images/linkedin.svg" /></a></li>
+              <li><a href="https://www.twitter.com/timicodes"><img src="/images/twitter.svg"/></a></li>
+              <li><a href="https://www.medium.com/timitejumola"><img src="/images/medium.svg"/></a></li>
+            </ul>
+          </div>
+          <Avatar />
         </div>
       </main>
-
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        <h1>TIMI TEJUMOLA</h1>
       </footer>
-
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -67,141 +44,162 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          margin-bottom: -8rem;
         }
-
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .wave-hand {
+          background-image: url(/images/emoji/wave.png);
+          margin-left: 10px;
+          cursor: pointer;
+]          -webkit-animation-duration: 1s;
+          animation-duration: 1s;
+          -webkit-animation-fill-mode: both;
+          animation-fill-mode: both;
+          -webkit-animation-name: wave;
+          animation-name: wave;
         }
+        @-webkit-keyframes wave {
+          from {
+            -webkit-transform: none;
+            transform: none;
+          }
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+          15% {
+            -webkit-transform: translate3d(-20%, 0, 0) rotate3d(0, 0, 1, -10deg);
+            transform: translate3d(-20%, 0, 0) rotate3d(0, 0, 1, -10deg);
+          }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+          30% {
+            -webkit-transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 7deg);
+            transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 7deg);
+          }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+          45% {
+            -webkit-transform: translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -10deg);
+            transform: translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -10deg);
+          }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+          60% {
+            -webkit-transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 5deg);
+            transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 5deg);
+          }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+          75% {
+            -webkit-transform: translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -2deg);
+            transform: translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -2deg);
+          }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+          to {
+            -webkit-transform: none;
+            transform: none;
           }
         }
-      `}</style>
 
+        @keyframes wave {
+          from {
+            -webkit-transform: none;
+            transform: none;
+          }
+
+          15% {
+            -webkit-transform: translate3d(-20%, 0, 0) rotate3d(0, 0, 1, -10deg);
+            transform: translate3d(-20%, 0, 0) rotate3d(0, 0, 1, -10deg);
+          }
+
+          30% {
+            -webkit-transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 7deg);
+            transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 7deg);
+          }
+
+          45% {
+            -webkit-transform: translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -10deg);
+            transform: translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -10deg);
+          }
+
+          60% {
+            -webkit-transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 5deg);
+            transform: translate3d(10%, 0, 0) rotate3d(0, 0, 1, 5deg);
+          }
+
+          75% {
+            -webkit-transform: translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -2deg);
+            transform: translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -2deg);
+          }
+
+          to {
+            -webkit-transform: none;
+            transform: none;
+          }
+        }
+
+        .hello,
+        .description {
+          text-align: center;
+          margin: 0;
+          font-weight: 300;
+        }
+        .description {
+          font-size: 2rem;
+          font-weight: 300;
+          margin: 0;
+        }
+        .description span {
+          font-weight: 800;
+        }
+        .section-two {
+          display: flex;
+          justify-content: space-between;
+        }
+        .section-two .links {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .resume-link {
+          padding: 0.56rem 2.2rem;
+          border: 2px solid #000000;
+          border-radius: 5px;
+          margin-top: 2rem;
+          font-weight: 800;
+          font-size: 0.7rem;
+        }
+        footer h1 {
+          font-size: 15rem;
+          margin: 0;
+          color: #787878;
+          opacity: 0.1;
+        }
+      `}</style>
       <style jsx global>{`
         html,
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+          font-family: Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
+        }
+        a {
+          text-decoration: none;
+          color: #000000;
+        }
+        ul {
+          display: flex;
+          margin-left: 0px;
+        }
+        li {
+          list-style: none;
+          padding: 1.5rem;
         }
       `}</style>
     </div>
